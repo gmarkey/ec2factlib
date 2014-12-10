43f2cbca19fbddda4fcceb2711f741958cb32921
David Baggerman <david.baggerman@fairfaxmedia.com.au>
2014-12-11 10:32:39 +1100

    Refactor and add comments

--- lib/facter/ec2facts.rb
+++ lib/facter/ec2facts.rb
@@ -7,6 +7,9 @@ require 'base64'
 require 'rexml/document'
 require 'json'
 
+# Escape URI form components.
+# URI.encode_www_form_component doesn't exist on Ruby 1.8, so we fall back to
+# URI.encode + gsub
 def uri_encode(component)
   if URI.respond_to? :encode_www_form_component
     URI.encode_www_form_component(component)
@@ -15,6 +18,7 @@ def uri_encode(component)
   end
 end
 
+# The instance document tells us our instance id, region, etc
 def get_instance_document
   url = URI.parse('http://169.254.169.254/latest/dynamic/instance-identity/document')
   response = Net::HTTP.get_response(url)
@@ -24,31 +28,27 @@ def get_instance_document
   return JSON.parse(response.body)
 end
 
-def get_instance_id
-  url = URI.parse('http://169.254.169.254/latest/meta-data/instance-id')
-  response = Net::HTTP.get_response(url)
-
-  return nil if response.code != "200"
-
-  return response.body
-end
-
+# If an IAM role is available for the instance, we will attempt to query it.
+# We return an empty Hash on failure. Keys may be available via enviroment
+# variables as a fallback.
 def get_instance_role
   url = URI.parse('http://169.254.169.254/latest/meta-data/iam/security-credentials/')
   response = Net::HTTP.get_response(url)
 
-  return nil if response.code != "200"
+  return {} if response.code != "200"
 
   body = response.body
 
   role = body.lines.first
   response = Net::HTTP::get_response(url+role)
 
-  return nil if response.code != "200"
+  return {} if response.code != "200"
 
   role = JSON.parse(response.body)
 end
 
+# Sign and send a request to the AWS REST API. Method is defined as an "Action" parameter.
+# parameters is an array because order is important for the signing process.
 def query(parameters, endpoint, access_key, secret_key, token = nil)
   timestamp = Time.now.utc.strftime('%Y-%m-%dT%H:%M:%SZ')
 
@@ -86,15 +86,10 @@ def query(parameters, endpoint, access_key, secret_key, token = nil)
 
   response = http.start { http.request(req) }
 
-  if response.code != "200"
-    $stderr.puts response
-    $stderr.puts response.body
-    exit 1
-  end
-
   response
 end
 
+# Queries the tags from the provided EC2 instance id.
 def query_instance_tags(instance_id, endpoint, access_key, secret_key, token = nil)
   parameters = [
     ['Action',            'DescribeInstances' ],
@@ -102,6 +97,11 @@ def query_instance_tags(instance_id, endpoint, access_key, secret_key, token = n
     ['Version',           '2014-10-01'        ],
   ]
   response = query(parameters, endpoint, access_key, secret_key, token)
+
+  if response.code != "200"
+    Facter.debug("DescribeInstances returned #{response.code} #{response.message}")
+    return {}
+  end
 
   doc = REXML::Document.new(response.body)
 
@@ -115,6 +115,7 @@ def query_instance_tags(instance_id, endpoint, access_key, secret_key, token = n
   return tags
 end
 
+# Queries the min/max/desired size of the provided autoscaling group.
 def query_autoscale_group(group_id, endpoint, access_key, secret_key, token)
   parameters = [
     ['Action',                            'DescribeAutoScalingGroups' ],
@@ -123,22 +124,58 @@ def query_autoscale_group(group_id, endpoint, access_key, secret_key, token)
   ]
   response = query(parameters, endpoint, access_key, secret_key, token)
 
+  if response.code != "200"
+    Facter.debug("DescribeAutoScalingGroups returned #{response.code} #{response.message}")
+    return {}
+  end
+
   doc = REXML::Document.new(response.body)
 
-  min_size = doc.get_elements('//MinSize')[0].text
-  max_size = doc.get_elements('//MaxSize')[0].text
-  desired  = doc.get_elements('//DesiredCapacity')[0].text
+  # Note: These params get merged into the facts Hash, so the keys should match the fact names
+  params = {}
 
-  return min_size, max_size, desired
+  min_size_elem = doc.get_elements('//MinSize')[0]
+  if min_size_elem.nil?
+    Facter.debug("No MinSize found for autoscaling group #{group_id}")
+    return nil
+  else
+    params['autoscaling_min_size'] = min_size_elem.text
+  end
+
+  max_size_elem = doc.get_elements('//MinSize')[0]
+  if max_size_elem.nil?
+    Facter.debug("No MinSize found for autoscaling group #{group_id}")
+    return nil
+  else
+    params['autoscaling_max_size'] = max_size_elem.text
+  end
+
+  desired_cap_elem = doc.get_elements('//MinSize')[0]
+  if desired_cap_elem.nil?
+    Facter.debug("No MinSize found for autoscaling group #{group_id}")
+    return nil
+  else
+    params['autoscaling_desired_capacity'] = desired_cap_elem.text
+  end
+
+  return params
 end
 
-if File.exists?('/etc/ec2_version')
+# Look for our EC2 instance ID, then query the tags assigned to the instance.
+# If there is an autoscaling group tag (aws:autoscaling:groupName), also query
+# the autoscaling group min/max/desired size parameters.
+def check_facts
   facts = {}
   open('/etc/ec2_version', 'r') do |io|
     facts['ec2_version'] = io.read.strip
   end
 
   instance    = get_instance_document
+  if instance.nil?
+    Facter.debug("Didn't get instance document from http://169.254.169.254/latest/dynamic/instance-identity/document")
+    return
+  end
+
   instance_id = instance['instanceId']
   region      = instance['region']
   role        = get_instance_role
@@ -147,8 +184,12 @@ if File.exists?('/etc/ec2_version')
   secret_key  = role['SecretAccessKey'] || ENV['AWS_SECRET_ACCESS_KEY']
   token       = role['Token']
 
-  tags = query_instance_tags(instance_id, "ec2.#{region}.amazonaws.com", access_key, secret_key, token)
+  if access_key.nil? || secret_key.nil?
+    Facter.debug("No authentication key available")
+    return
+  end
 
+  tags = query_instance_tags(instance_id, "ec2.#{region}.amazonaws.com", access_key, secret_key, token)
   tags.each do |tag, value|
     next if tag.start_with?('aws:')
 
@@ -160,27 +201,26 @@ if File.exists?('/etc/ec2_version')
 
     facts['autoscaling_group_name'] = autoscale_group
 
-    min_size, max_size, desired = query_autoscale_group(autoscale_group, "autoscaling.#{region}.amazonaws.com", access_key, secret_key, token)
-    facts['autoscaling_min_size']         = min_size
-    facts['autoscaling_max_size']         = max_size
-    facts['autoscaling_desired_capacity'] = desired
+    asg_params = query_autoscale_group(autoscale_group, "autoscaling.#{region}.amazonaws.com", access_key, secret_key, token)
+    facts = facts.merge(asg_params)
   end
 
   if tags.has_key? 'aws:cloudformation:stack-name'
     facts['cloudformation_stack_name'] = tags['aws:cloudformation:stack-name']
   end
 
-  if defined? Facter
-    facts.each do |fact, value|
-      Facter.add(fact) do
-        setcode { value }
-      end
-    end
-  elsif binding.respond_to? :pry
-    binding.pry
-  else
-    facts.each do |fact, value|
-      puts "#{fact} => #{value}"
+  facts.each do |fact, value|
+    Facter.add(fact) do
+      setcode { value }
     end
   end
+rescue rescue StandardError => e
+  Facter.debug("Unhandled #{e.class}: #{e.message}")
+end
+
+# This file seems to exist on our EC2 instances. There may be a better way to
+# determine if we are running on EC2.
+# We mostly want to avoid waiting for http://169.254.169.254/ to time out if we are not on EC2.
+if File.exists?('/etc/ec2_version')
+  check_facts
 end
